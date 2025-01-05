4796b9756ffcf36cb675c10ab7fe4c30ec6a6b87
Rsirp0c <haorancheng@brandeis.edu>
2025-01-06 01:45:51 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,11 +6,14 @@ app = FastAPI(title="Vector Search and Chat API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-                   "https://jobs-chatbot.vercel.app"],
+    allow_origins=["https://jobs-chatbot.vercel.app/","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def read_root():
+    return {"message": "Backend is running"}
 
 app.include_router(api_router, prefix="/api/v1")
