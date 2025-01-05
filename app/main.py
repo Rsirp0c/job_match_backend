3d103791e76a363827a96787c1f3bc1f73bf185e
Rsirp0c <haorancheng@brandeis.edu>
2025-01-06 01:57:04 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Vector Search and Chat API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://jobs-chatbot.vercel.app/","http://localhost:3000"],
+    allow_origins=["https://jobs-chatbot.vercel.app","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
