57c0ba3811a2ccbaeb79ef645aa2d281428ff727
Rsirp0c <haorancheng@brandeis.edu>
2025-01-21 22:40:49 -0500

    render

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import router as api_router
 import logging
+import os
+import uvicorn
 
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
@@ -20,3 +22,7 @@ async def read_root():
     return {"message": "Backend is running"}
 
 app.include_router(api_router, prefix="/api/v1")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
